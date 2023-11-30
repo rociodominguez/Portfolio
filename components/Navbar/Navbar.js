7769622d056b0aeed8a6d660d149a771c8d7beea
Rocío Domínguez <rociodominguezcontacto@gmail.com>
2023-11-30 21:18:07 +0100

    v2

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -16,11 +16,31 @@ const createNavbar = () => {
     const a = document.createElement('a');
     a.textContent = item.title;
     a.href = item.href;
+
+    a.addEventListener('click', () => {
+      ul.classList.remove('show');
+    });
+
     li.appendChild(a);
     ul.appendChild(li);
   });
 
   nav.appendChild(ul);
+
+  const toggleButton = document.createElement('button');
+  toggleButton.classList.add('toggle-button', 'right');
+
+  const img = document.createElement('img');
+  img.src = "https://res.cloudinary.com/dfufuprrn/image/upload/v1701367717/toggle_ulmx6y.png";
+  img.classList.add('my-toggle');
+  toggleButton.appendChild(img);
+
+  toggleButton.addEventListener('click', () => {
+    ul.classList.toggle('show');
+  });
+
+  nav.appendChild(toggleButton);
+
   return nav;
 };
 
